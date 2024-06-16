d2ed9c1ca6ee08aad7815a9ccff7ffa28508bc07
everythingapplejj <everythingapplejj@gmail.com>
2024-06-16 15:56:30 -0500

    Router set

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const app = express()
+// views ejs view 
+
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
     console.log('here')
-    res.status(500).json({message: "error"}) // usage with API
+    res.render('index', { text: 'World' }) 
    
 }) //route
 
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter) // it is like calling the mini applications
 
 app.listen(3001)
 
